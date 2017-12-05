6d7790e9dc8169a2283634f7e65dc3b4a80e919a
Nawaf Abdullah <nawaf97k@gmail.com>
2017-12-05 16:33:59 -0500

    Defined spin function of Pulsar

--- PulsarNavigation2D.py
+++ PulsarNavigation2D.py
@@ -1,27 +1,54 @@
 import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-class Ship:
+class Bounds:
+    lo = -10
+    hi = 10
+
+    def set_bounds(self, low, high):
+        self.lo = low
+        self.hi = high
+
+
+class Ship(Bounds):
 
     x = 0
     y = 0
 
     def generate_coordinates(self):
-        self.x = random.uniform(-1500, 1500)
-        self.y = random.uniform(-1500, 1500)
+        self.x = random.uniform(self.lo, self.hi)
+        self.y = random.uniform(self.lo, self.hi)
 
 
-class Pulsar:
+class Pulsar(Bounds):
 
-    x_coords = []
-    y_coords = []
-    verf_num = 0
+    x_c = 0
+    y_c = 0
+    T = 100
 
-    def generate(self):
-        numPulsars = int(input("Number of pulsars > "))
-        self.verf_num = numPulsars
-        for it in range(numPulsars+1):
-            self.x_coords.append(random.uniform(-1500, 1500))
-            self.y_coords.append(random.uniform(-1500, 1500))
+    def set_coordinates(self, xi, yi):
+        self.x_c = xi
+        self.y_c = yi
+
+    def set_period(self, T_o):
+        self.T = T_o
+
+    def spin(self, t=10000):
+        plt.ion()
+        for i in range(t):
+            func = lambda x: (x - self.x_c)*math.tan(2 * np.pi * i / self.T) + self.y_c
+            y = np.array(range(self.lo, self.hi))
+            x = func(y)
+            plt.plot(x, y)
+            plt.xlim([-1, 1])
+            plt.ylim([-1, 1])
+            plt.pause(0.00005)
+            plt.cla()
+        while True:
+            plt.pause(0.00005)
 
 
+Pulsar.spin(Pulsar())
