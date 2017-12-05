137810f47810603b4d2b220d831fe003268ba003
Nawaf Abdullah <nawaf97k@gmail.com>
2017-12-05 00:58:18 -0500

    Initial definition of Pulsar and Ship

--- PulsarNavigation2D.py
+++ PulsarNavigation2D.py
@@ -1,2 +1,27 @@
+import random
+
+
 class Ship:
-    
+
+    x = 0
+    y = 0
+
+    def generate_coordinates(self):
+        self.x = random.uniform(-1500, 1500)
+        self.y = random.uniform(-1500, 1500)
+
+
+class Pulsar:
+
+    x_coords = []
+    y_coords = []
+    verf_num = 0
+
+    def generate(self):
+        numPulsars = int(input("Number of pulsars > "))
+        self.verf_num = numPulsars
+        for it in range(numPulsars+1):
+            self.x_coords.append(random.uniform(-1500, 1500))
+            self.y_coords.append(random.uniform(-1500, 1500))
+
+
