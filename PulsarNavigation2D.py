c5e7d1c7bdb1c9a2cffb38cc9ce498ee203e325d
Nawaf Abdullah <nawaf97k@gmail.com>
2017-12-05 17:28:54 -0500

    Initial definition of Pulsar and Ship

--- PulsarNavigation2D.py
+++ PulsarNavigation2D.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Bounds:
+class Space:
+    VerfNum = 0
     lo = -1000
     hi = 1000
 
@@ -13,23 +14,50 @@ class Bounds:
         self.hi = high
 
 
-class Ship(Bounds):
+class Ship(Space):
 
     x = 0
     y = 0
+
+    def set_coordinates(self, xo, yo):
+        self.x = xo
+        self.y = yo
 
     def generate_coordinates(self):
         self.x = random.uniform(self.lo, self.hi)
         self.y = random.uniform(self.lo, self.hi)
 
+    def verify_location(self, numPulsar, pulsar):
+        self.uniques=[]
 
-class Pulsar(Bounds):
+        for x in range(numPulsar):
+            self.uniques.append(x)
+
+        for i in range(numPulsar):
+            for j in range(numPulsar):
+                if i == j:
+                    continue
+                if pulsar[i].T == pulsar[j].T:
+                    Space.VerfNum -= 1
+                    self.uniques.remove(i)
+                    self.unique.remove[j]
+        return Space.VerfNum
+
+    def angleTo(self, pulsar):
+        return math.atan((pulsar.get_y() - self.y)/(pulsar.get_x()-self.x))
+
+    def triangulate(self):
+
+
+
+class Pulsar(Space):
 
     x_c = 0
     y_c = 0
     T = 100
 
     def __init__(self, xi, yi, T_o):
+
         self.x_c = xi
         self.y_c = yi
         self.T = T_o
@@ -47,5 +75,20 @@ class Pulsar(Bounds):
             plt.cla()
         while True:
             plt.pause(0.00005)
+
+    def get_period(self):
+        return self.T
+
+    def get_y(self):
+        return self.y_c
+
+    def get_x(self):
+        return self.x_c
+
+
+num_pulsars = int(input("Enter number of pulsars > "))
+Space.VerfNum = num_pulsars
+pulsars = [Pulsar(float(input("Pulsar x-coord > ")), float(input("y-coord > ")), float(input("Period > ")))
+           for i in range(num_pulsars)]
 
 
